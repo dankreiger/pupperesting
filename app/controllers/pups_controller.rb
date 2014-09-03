2e7afd9d8afe62ebb8dd3126707ae1aa8615556c
Dan Kreiger <dan@dankreiger.com>
2014-09-03 20:47:37 +0200

    Add pins/users association

--- app/controllers/pups_controller.rb
+++ app/controllers/pups_controller.rb
@@ -1,45 +1,38 @@
 class PupsController < ApplicationController
   before_action :set_pup, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
-  
   def index
     @pups = Pup.all
   end
 
-
   def show
   end
 
-
   def new
-    @pup = Pup.new
+    @pup = current_user.pups.build
   end
-
 
   def edit
   end
 
-
   def create
-    @pup = Pup.new(pup_params)
-
+    @pup = current_user.pups.build(pup_params)
     if @pup.save
       redirect_to @pup, notice: 'Pup was successfully created.'
     else
-      render :new
+      render action: 'new'
     end
   end
-
 
   def update
     if @pup.update(pup_params)
       redirect_to @pup, notice: 'Pup was successfully updated.'
     else
-      render :edit 
+      render action: 'edit'
     end
   end
-
-  
 
   def destroy
     @pup.destroy
@@ -52,8 +45,13 @@ class PupsController < ApplicationController
       @pup = Pup.find(params[:id])
     end
 
+    def correct_user
+      @pup = current_user.pups.find_by(id: params[:id])
+      redirect_to pups_path, notice: "Not authorized to edit this pup" if @pup.nil?
+    end
+
     # Never trust parameters from the scary internet, only allow the white list through.
     def pup_params
-      params.require(:pup).permit(:description)
+      params.require(:pup).permit(:description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903032220) do
+ActiveRecord::Schema.define(version: 20140903171523) do
 
   create_table "pups", force: true do |t|
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "pups", ["user_id"], name: "index_pups_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
