9a14b5b6f203c32c041f46a6667781e80d5f6845
Dan Kreiger <dan@dankreiger.com>
2014-09-04 20:08:53 +0200

    Imagemagick/paperclip

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,4 +37,7 @@ Rails.application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # Imagemagick/Paperclip
+  Paperclip.options[:command_path] = "/usr/local/bin/"
 end
---
+++ db/migrate/20140904132435_add_attachment_image_to_pups.rb
@@ -0,0 +1,11 @@
+class AddAttachmentImageToPups < ActiveRecord::Migration
+  def self.up
+    change_table :pups do |t|
+      t.attachment :image
+    end
+  end
+
+  def self.down
+    remove_attachment :pups, :image
+  end
+end
