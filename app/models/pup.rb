5926fcc89d7571b4d11469f9bcb60ec712848190
Dan Kreiger <dan@dankreiger.com>
2015-04-19 17:42:34 -0400

    fix model

--- app/models/pup.rb
+++ app/models/pup.rb
@@ -3,7 +3,7 @@ class Pup < ActiveRecord::Base
   
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_file_name :image, :matches => [/png\Z/, /jpe?g\Z/, /gif\Z/]
 
   validates :image, presence: true
   
